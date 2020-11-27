b63783cbbd283633e536341762595c8f61a347a7
HAPPIESTMINDS\Meena.sriram <meena.sriram@happiestminds.com>
2020-11-27 17:11:29 +0530

    Working Poc

--- src/app/play-ar/play-ar.component.ts
+++ src/app/play-ar/play-ar.component.ts
@@ -17,6 +17,7 @@ export class PlayArComponent implements OnInit {
   };
   image: any;
   url: any;
+  color: any;
   
   ngOnInit(): void {
     console.log ("first step");
@@ -28,14 +29,33 @@ export class PlayArComponent implements OnInit {
         console.log(this.image); // popular
         var sceneEl = document.querySelector('a-scene');
         var newObj = sceneEl.querySelector('#hmnft');
+        var entity =  document.querySelector('a-entity');
+        var aframeObj = entity.querySelector("#refresh-button")
     
-        if (this.image === "pinball") {
-          newObj.setAttribute('url', 'https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/data/dataNFT/pinball');
-          this.url = "https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/data/dataNFT/pinball";
-        } else  {
-          newObj.setAttribute('url', 'https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex');
-          this.url ="https://arjs-cors-proxy.herokuapp.com/https://raw.githack.com/AR-js-org/AR.js/master/aframe/examples/image-tracking/nft/trex/trex-image/trex";
-        }
+        if (this.image === "azim") {
+          newObj.setAttribute('url', 'https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/azim/azim');
+          this.url = "https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/azim/azim";
+          aframeObj.setAttribute('material', 'color:blue');
+          this.color ="color:blue";
+
+        } else if (this.image==='dalai') {
+          newObj.setAttribute('url', 'https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/dalai/dalai');
+          this.url ="https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/dalai/dalai";
+          aframeObj.setAttribute('material', 'color:green');
+          this.color ="color:green";
+        } else if (this.image==='shoedog') {
+          newObj.setAttribute('url', 'https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/shoedog/shoedog');
+          this.url ="https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/shoedog/shoedog";
+          aframeObj.setAttribute('material', 'color:red');
+          this.color ="color:red";
+        } else if (this.image==='whomoved') {
+          newObj.setAttribute('url', 'https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/whomoved/whomoved');
+          this.url ="https://arjs-cors-proxy.herokuapp.com/https://cdn.statically.io/gh/meena-sriram/ar-project/0f827ebb/src/assets/whomoved/whomoved";
+          aframeObj.setAttribute('material', 'color:yellow');
+          this.color ="color:yellow";
+        } 
+
+
         console.log ("url -->", this.url)
       }
     );
